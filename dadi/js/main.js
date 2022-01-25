b20939038eeb6c45cde86e11a8bba48cedd50823
pietroline <pietrobarberi@hotmail.it>
2022-01-25 16:52:40 +0100

    dadi html css js

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -4,9 +4,21 @@ const randomUser = Math.floor((Math.random()*6)+1);
 const randomPC = Math.floor((Math.random()*6)+1);
 
 if (randomUser == randomPC){
-    alert(`Numero utente: ${randomUser} \n Numero PC: ${randomPC} \n Nessun vincitore, parità`);
+    // console.log(`Numero utente: ${randomUser} \n Numero PC: ${randomPC} \n Nessun vincitore, parità`);
+    document.getElementById("vincitore").innerHTML = "Parità";
 }else if (randomUser > randomPC){
-    alert(`Numero utente: ${randomUser} \n Numero PC: ${randomPC} \n Il vincitore è l'utente`);
+    // console.log(`Numero utente: ${randomUser} \n Numero PC: ${randomPC} \n Il vincitore è l'utente`);
+    document.getElementById("vincitore").innerHTML = "Il vincitore è Utente"
 }else{
-    alert(`Numero utente: ${randomUser} \n Numero PC: ${randomPC} \n Il vincitore è il PC`);
+    // console.log(`Numero utente: ${randomUser} \n Numero PC: ${randomPC} \n Il vincitore è il PC`);
+    document.getElementById("vincitore").innerHTML = "Il vincitore è PC"
 }
+
+// append del dado utente, per inserire classe d-block
+document.getElementById("dadoUser_faccia-" + randomUser).classList.add("d-block");
+
+// append del dado PC, per inserire classe d-block
+document.getElementById("dadoPC_faccia-" + randomPC).classList.add("d-block");
+
+
+
