a92968714b305e1d8dda37840f2ee9614e52cea3
pietroline <pietrobarberi@hotmail.it>
2022-01-25 17:37:07 +0100

    mail html css js

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,21 +1,51 @@
 
 const emailRegistrata = ["topolino@disney.it", "paperino@disney.it", "pippo@disney.it", "pluto@disney.it"];
+const passwordEmailRegistrata = ["topolinoDisney21", "paperinoDisney23", "pippoDisney27", "plutoDisney36"];
 
-const email = prompt("Inserisci la tua email");
+let accedi = document.getElementById("accedi");
 
-//l'utilizzo della variabile check è necessario per evitare di lanciare un alert per ogni iterazione in cui email != emailRegistrata
-let check = 0;
+accedi.addEventListener("click",
 
-for(let i=0; i<emailRegistrata.length; i++){
+    function(){
 
-    if(email == emailRegistrata[i]){
-        alert("L'email inserita è autorizzata");
-    }else{
-        check += 1;
+        // prelevo email inserita dall'utente e metto tutti i caratteri di stringa minuscoli, in maniera tale che non ci siano problemi nel confronto con le email registrate
+        let email = document.getElementById("email").value;
+        email = email.toLowerCase();
+
+        // prelevo password inserita dall'utente
+        let password = document.getElementById("password").value;
+        
+        //l'utilizzo della variabile check è necessario per evitare di lanciare un alert per ogni iterazione in cui email != emailRegistrata
+        let check = 0;
+
+        for(let i=0; i<emailRegistrata.length; i++){
+
+            if(email == emailRegistrata[i]){
+                if(password == passwordEmailRegistrata[i]){
+                    alert("L'email e password inserite sono corrette, sei autorizzato!");
+                }else{
+                    alert("Password sbagliata!");
+                }
+            }else{
+                check += 1;
+            }
+            
+            if(check == emailRegistrata.length){
+                alert("L'email NON è registrata!");
+            }
+            
+        }
     }
-    
-    if(check == emailRegistrata.length){
-        alert("L'email NON è registrata!");
+
+);
+
+let annulla = document.getElementById("annulla");
+
+annulla.addEventListener("click",
+
+    function(){
+
+        document.getElementById("annulla").value = "";
     }
-    
-}
+
+);
